f471845debc77759202c4a0d9c09786f3e3401e7
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-07-16 13:11:16 +0200

    Mise en place du javascript du formulaire et mise en place de validation du formulaire avant envoi.

--- src/form/form.js
+++ src/form/form.js
@@ -1,2 +1,35 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
+
+const form = document.querySelector("form");
+const errorElement = document.querySelector("#errors");
+let errors = [];
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    const json = JSON.stringify(article);
+    // Requête à implémenter
+  }
+});
+
+const formIsValid = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push("Vous devez renseigner tous les champs");
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHTML = "";
+    errors.forEach((e) => {
+      errorHTML += `<li>${e}</li>`;
+    });
+    errorElement.innerHTML = errorHTML;
+    return false;
+  } else {
+    errorElement.innerHTML = "";
+    return true;
+  }
+};
