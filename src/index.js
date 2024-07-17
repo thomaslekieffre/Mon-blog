0de78a04f7881cd569cd204d38a87e648b59fe48
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-07-17 10:30:48 +0200

    Mise en place de la suppression d'article et fix pour les articles ou on passe des lignes et responsive ajouté. Partie 1 du TP finie.

--- src/index.js
+++ src/index.js
@@ -25,6 +25,26 @@ const createArticles = (articles) => {
   });
   articleContainerElement.innerHTML = "";
   articleContainerElement.append(...articlesDOM);
+  const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+        const response = await fetch(
+          `https://restapi.fr/api/article/${articleId}`,
+          {
+            method: "DELETE",
+          }
+        );
+        const body = await response.json();
+        console.log(body);
+        fetchArticle();
+      } catch (e) {
+        console.log("e : ", e);
+      }
+    });
+  });
 };
 
 const fetchArticle = async () => {
