6a31518efb42cd4c71ca0cd429628c4e518847f4
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-07-22 11:04:08 +0200

    Mise en place de la modification de l'article

--- src/form/form.js
+++ src/form/form.js
@@ -5,11 +5,48 @@ const form = document.querySelector("form");
 const errorElement = document.querySelector("#errors");
 const btnCancel = document.querySelector(".btn-secondary");
 let errors = [];
+let articleId;
+
+// Nous allons créer une fonction asynchrone que nous invoquons de suite.
+// Nous parsons l’URL de la page et vérifions si nous avons un paramètre id.
+// Si nous avons un id, nous récupérons l’article correspondant.
+const initForm = async () => {
+  const params = new URL(window.location.href);
+  articleId = params.searchParams.get("id");
+  if (articleId) {
+    const response = await fetch(`https://restapi.fr/api/article/${articleId}`);
+    if (response.status < 300) {
+      const article = await response.json();
+      fillForm(article);
+    }
+  }
+};
+
+initForm();
+
+// Nous remplissons tous les champs de notre formulaire en créant des références
+// et en utilisant les informations récupérées du serveur.
+const fillForm = (article) => {
+  const author = document.querySelector('input[name="author"]');
+  const img = document.querySelector('input[name="img"]');
+  const category = document.querySelector('input[name="category"]');
+  const title = document.querySelector('input[name="title"]');
+  const content = document.querySelector("textarea");
+  author.value = article.author || "";
+  img.value = article.img || "";
+  category.value = article.category || "";
+  title.value = article.title || "";
+  content.value = article.content || "";
+};
 
 btnCancel.addEventListener("click", () => {
   window.location.assign("/index.html");
 });
 
+// Lorsque nous éditons, nous ne créons pas de nouvelle ressource sur le serveur.
+// Nous n’utilisons donc pas une requête POST mais une requête PATCH.
+// Pas PUT car nous ne remplaçons pas la ressource distante (nous gardons
+// la date de création et l’id).
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -17,15 +54,24 @@ form.addEventListener("submit", async (event) => {
   if (formIsValid(article)) {
     try {
       const json = JSON.stringify(article);
-      const response = await fetch("https://restapi.fr/api/article", {
-        method: "POST",
-        body: json,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const body = await response.json();
-      console.log(body);
+      let response;
+      if (articleId) {
+        response = await fetch(`https://restapi.fr/api/article/${articleId}`, {
+          method: "PATCH",
+          body: json,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      } else {
+        response = await fetch("https://restapi.fr/api/article", {
+          method: "POST",
+          body: json,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+      }
       if (response.status < 299) {
         window.location.assign("/index.html");
       }
--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const createArticles = (articles) => {
 </p>
 <div class="article-actions">
   <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
+  <button class="btn btn-primary" data-id=${article._id} >Modifier</button>
 </div>
 `;
     return articleDOM;
@@ -33,6 +34,14 @@ const createArticles = (articles) => {
   articleContainerElement.innerHTML = "";
   articleContainerElement.append(...articlesDOM);
   const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+  const editButtons = articleContainerElement.querySelectorAll(".btn-primary");
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const target = event.target;
+      const articleId = target.dataset.id;
+      window.location.assign(`/form/form.html?id=${articleId}`);
+    });
+  });
   deleteButtons.forEach((button) => {
     button.addEventListener("click", async (event) => {
       try {
